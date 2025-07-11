94611d67af98de9d8d06ca91180014ee916cec6a
LeonelC98 <Leonel18costamagna@gmail.com>
2025-07-11 16:41:49 -0300

    agregado loguers y manejo de errores(middleware)

--- app.js
+++ app.js
@@ -4,16 +4,26 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/Blogs')
-
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
+mongoose.set('strictQuery', false);
+
+logger.info('Estableciendo conexion con la base de datos', config.MONGODB_URI)
 
 mongoose.connect(config.MONGODB_URI)
-
+.then(()=>{
+    logger.info('Establecida conexion con la base de datos')
+}).catch((error)=>{
+    logger.error('Error al conectar con la base de datos ', error.message)
+})
 
 app.use(cors())
 app.use(express.json())
-
+app.use(middleware.requestLogger)
 app.use('/api/blogs',blogsRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app;
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./app')
 const config = require('./utils/config')
-
+const logger = require('./utils/logger')
 app.listen(config.PORT, () => {
-  console.log(`http://localhost:${config.PORT}`)
+  logger.info(`http://localhost:${config.PORT}`)
 })
