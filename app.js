b1f0b5f5ccf967ff7650928be3a40f1893a868a6
LeonelC98 <Leonel18costamagna@gmail.com>
2025-07-11 15:36:08 -0300

    refactorizado los controladores en sus modulo respectivo

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const blogsRouter = require('./controllers/Blogs')
+
 const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+
 
 mongoose.connect(config.MONGODB_URI)
 
@@ -12,22 +14,6 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs',blogsRouter)
 
 module.exports = app;
