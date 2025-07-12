5bff47b59209c9dd8fd496173d700acad87cef76
LeonelC98 <Leonel18costamagna@gmail.com>
2025-07-12 15:43:07 -0300

    ejersicion 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,15 @@
+
 const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  if (!blogs.length){
+    return 0
+  }
+  return blogs.reduce((total, blog) => total + blog.likes, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,totalLikes
 }
